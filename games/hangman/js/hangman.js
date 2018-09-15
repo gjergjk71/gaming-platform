408a58d4e81b1d69d4d750668bc8fb2d3fe1d3d1
Gjergj Kadriu <gjergjk71@gmail.com>
2018-09-15 14:35:12 +0200

    play() function gets random word from list

--- games/hangman/js/hangman.js
+++ games/hangman/js/hangman.js
@@ -2,9 +2,15 @@ window.onload = function () {
 	const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 			'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 			't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	const wordsList = ["solid","tiger","try","buzz",
+						"seal","pen","deer","foregoing",
+						"mug","sweater","next","great"]
+	var guessesLeft;
+	var word;
 	function play() {
-		var guessesLeft = 10; // 
-		var word; //Word to guess
+		guessesLeft = 10; // 
+		word = wordsList[Math.floor(Math.random() * wordsList.length)]; //Word to guess
 		};
-
+	play();
+	console.log(word);
 };
