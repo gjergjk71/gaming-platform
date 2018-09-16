a9aa326a7eaa51f9ba55e052dc6be03f34d8bc67
gjergjk71 <gjergjk71@gmail.com>
2018-09-16 13:21:34 +0200

    Created guessLetter function

--- games/hangman/js/hangman.js
+++ games/hangman/js/hangman.js
@@ -11,12 +11,34 @@ window.onload = function () {
 	// Create alphabet ul-li
 	var alphabet_ui = document.createElement('ul');
 	for (var i=0;i < alphabet.length;i++) {
+		var letter = alphabet[i];
 		var li = document.createElement("li");
-		li.innerHTML = `<button id="alphabet_letter" class='btn btn-primary'> ${alphabet[i]} </button>`;
+		//li.innerHTML = `<button id="alphabet_letter" class='btn btn-primary'> ${alphabet[i]} </button>`;
+		button = document.createElement("button");
+		button.id = letter;
+		button.classList.add = "btn";
+		button.classList.add = "btn-primary";
+		button.innerHTML = letter;
+		button.onclick = (MouseEvent) => {
+			guessLetter(MouseEvent);
+		};
+		li.appendChild(button);
 		alphabet_ui.appendChild(li);
 	};
 	document.getElementById("alphabet").appendChild(alphabet_ui);
 	//
+	function guessLetter(MouseEvent) {
+		var letter = MouseEvent["target"].id;
+		if (word.split('').indexOf(letter) >= 0) {
+			console.log("true)");
+			for (var i=0;i<word.length;i++) {
+				if (letter == word[i]) {
+					guessedLetters[letter] = true;
+				}
+			}
+			showGuessedLetters();
+		}
+	}
 	function multiplyString(str,times) {
 		multipliedString = ""
 		for (x=0;x<times;x++) {
@@ -24,15 +46,16 @@ window.onload = function () {
 		};
 		return multipliedString;
 	};
-	function showGuessedLetters(gLetters) {
+	function showGuessedLetters() {
 		show = ""
-		for (var key in gLetters){
-			if (!gLetters[key]){
+		for (var key in guessedLetters){
+			if (!guessedLetters[key]){
 				show += "_ ";
 			} else {
 				show += key;
 			};
 		document.getElementById("guessed_letters").innerHTML=show;
+		console.log(guessedLetters);
 		};
 	};
 	function play() {
@@ -43,8 +66,7 @@ window.onload = function () {
 		for (var i=0;i<word.length;i++) {
 			guessedLetters[word[i]] = false;
 		};
-		console.log(guessedLetters);
-		showGuessedLetters(guessedLetters)
+		showGuessedLetters()
 		};
 	
 	play();
