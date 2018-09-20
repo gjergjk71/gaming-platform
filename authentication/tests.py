d867016c71c79ac3e80097d8cca2b8d4125e46a4
gjergjk71 <gjergjk71@gmail.com>
2018-09-20 16:10:07 +0200

    Created TestAuthentication TestCase

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from django.shortcuts import reverse
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class TestAuthentication(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user("testing","testing@gmail.com","testing")
