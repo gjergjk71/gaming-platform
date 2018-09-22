b94a4fad0e7419bfed0bfc3d109191a1d827858c
Gjergj Kadriu <gjergjk71@gmail.com>
2018-09-22 18:39:17 +0200

    Minor changes

--- models.py
+++ models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class game(models.Model):
+class Game(models.Model):
 	id = models.AutoField(primary_key=True)
-	GameName= models.CharField(max_length=50)
-	url=models.TextField()
+	GameName = models.CharField(max_length=50)
+	url = models.TextField()
 
 
-class conn(models.Model):
-	id=models.AutoField(primary_key=True)
+class Conn(models.Model):
+	id = models.AutoField(primary_key=True)
 	userId = models.ForeignKey(User,on_delete=models.CASCADE)
-	gameId=models.ForeignKey(game,on_delete=models.CASCADE)
-	score=models.IntegerField()
+	gameId = models.ForeignKey(Game,on_delete=models.CASCADE)
+	score = models.IntegerField()
 
-class Userprofile(models.Model):
+class UserProfile(models.Model):
 	id = models.AutoField(primary_key=True)
-	Userprofile= models.ForeignKey(User,on_delete=models.CASCADE,unique=True)
-	profile_image =models.ImageField(upload_to="profile_photos", blank=True, null=True)
+	user = models.ForeignKey(User,on_delete=models.CASCADE,unique=True)
+	profile_image = models.ImageField(upload_to="profile_photos", blank=True, null=True)
