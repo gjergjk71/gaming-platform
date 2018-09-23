aa447e615bd94de6b8a05bcd67f3ba0439b6f700
gjergjk71 <gjergjk71@gmail.com>
2018-09-23 23:06:20 +0200

    Created send_message_api view

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect,reverse
 from django.http import HttpResponse
 from .models import GameMessage
+from user_profile.models import UserProfile
 from games.models import Game
 from django.core import serializers
 # Create your views here.
@@ -15,4 +16,19 @@ def get_messages_api(request,game_id):
 	game = get_object_or_404(Game,pk=game_id)
 	game_messages = GameMessage.objects.filter(game=game)
 	data = serializers.serialize('json', game_messages)
+	return HttpResponse(data,content_type="application/json")
+
+def send_message_api(request,game_id):
+	if request.method == "GET":
+		return redirect(reverse("game_chat",args=[game_id]))
+	try:
+		game = get_object_or_404(Game,pk=game_id)
+		user_profile = get_object_or_404(UserProfile,user=request.user)
+		content = request.POST.get("content")
+		GameMessage.objects.create(game=game,
+								   user_profile=user_profile,
+								   content=content)
+		data = serializers.serialize('json', {"successfull":True})
+	except:
+		data = serializers.serialize('json', {"successfull":False})
 	return HttpResponse(data,content_type="application/json")
