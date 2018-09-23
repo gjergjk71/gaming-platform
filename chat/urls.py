5edc08fd6588cbc69f07d30ca21abb0c27e8c96c
gjergjk71 <gjergjk71@gmail.com>
2018-09-23 20:01:27 +0200

    Created get_messages_api view and path

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path("game_chat/<int:game_id>",views.game_chat,name="game_chat"),
+	path("get_messages_api/<int:game_id>",views.get_messages_api,name="get_messages_api"),
 ]
