b195344dae3d87171dc280b59e236c93799ed4e4
gjergjk71 <gjergjk71@gmail.com>
2018-09-23 19:48:15 +0200

    Created game_chat view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import GameMessage
+from games.models import Game
 # Create your views here.
+
+def game_chat(request,game_id):
+	game = get_object_or_404(Game,pk=game_id)
+	game_messages = GameMessage.objects.filter(game=game)
+	context = {"game":game,"game_messages":game_messages}
+	return render(request,"chat/chat.html",context)
