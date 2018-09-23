d01311d6bd355c67387ed0fe647e54d2ffc271c6
gjergjk71 <gjergjk71@gmail.com>
2018-09-23 19:19:37 +0200

    Moved Score to games.models

--- games/models.py
+++ games/models.py
@@ -6,3 +6,7 @@ class Game(models.Model):
 	GameName = models.CharField(max_length=50)
 	url = models.CharField(max_length=500)
 
+class Score(models.Model):
+	userId = models.ForeignKey(User,on_delete=models.CASCADE)
+	gameId = models.ForeignKey(Game,on_delete=models.CASCADE)
+	score = models.IntegerField()
--- user_profile/models.py
+++ user_profile/models.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE,unique=True)
 	profile_image = models.ImageField(upload_to="profile_photos", blank=True, null=True)
+
