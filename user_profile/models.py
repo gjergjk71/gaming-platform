9b580a09835d4b36c6b1731d880b868d4547a1a9
gjergjk71 <gjergjk71@gmail.com>
2018-09-23 19:10:36 +0200

    Moved UserProfile to user_profile.models

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE,unique=True)
+	profile_image = models.ImageField(upload_to="profile_photos", blank=True, null=True)
