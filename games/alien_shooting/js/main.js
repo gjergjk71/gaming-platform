6d727da9087fe6a79e24e3cf4f9c1a4c32871bdb
gjergjk71 <gjergjk71@gmail.com>
2018-09-23 13:10:54 +0200

    Created Arc class

--- games/alien_shooting/js/main.js
+++ games/alien_shooting/js/main.js
@@ -1,28 +1,59 @@
 class Rectangle{
-			constructor(ctx,x,y,w,h,
-						strokeStyle,
-						fillStyle,
-						speed)
-			{
-				this.ctx = ctx;
-				this.strokeStyle = strokeStyle;
-				this.fillStyle = fillStyle;
-				this.speed = speed;
-				this.x = x;
-				this.y = y;
-				this.w = w;
-				this.h = h;
-			};
-			get draw() {
-				return this.reDraw();
-			}
-			reDraw(){
-				this.ctx.beginPath();
-				this.ctx.rect(this.x,this.y,this.w,this.h);
-				this.ctx.strokeStyle = this.strokeStyle;
-				this.ctx.fillStyle = this.fillStyle;
-				this.ctx.fill();
-				this.ctx.stroke();
-				this.ctx.closePath();				
-			}	
-		}
+	constructor(ctx,x,y,w,h,
+				strokeStyle,
+				fillStyle,
+				speed)
+	{
+		this.ctx = ctx;
+		this.strokeStyle = strokeStyle;
+		this.fillStyle = fillStyle;
+		this.speed = speed;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	};
+	get draw() {
+		return this.reDraw();
+	}
+	reDraw(){
+		this.ctx.beginPath();
+		this.ctx.rect(this.x,this.y,this.w,this.h);
+		this.ctx.strokeStyle = this.strokeStyle;
+		this.ctx.fillStyle = this.fillStyle;
+		this.ctx.fill();
+		this.ctx.stroke();
+		this.ctx.closePath();				
+	}	
+}
+class Arc{
+	constructor(ctx,x,y,r,sAngle,eAngle,
+				strokeStyle,
+				fillStyle,
+				speed,
+				counterclockwise=false)
+	{
+		this.ctx = ctx;
+		this.strokeStyle = strokeStyle;
+		this.fillStyle = fillStyle;
+		this.speed = speed;
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.sAngle = sAngle;
+		this.eAngle = eAngle;
+		this.counterclockwise = counterclockwise;
+	}
+	get draw() {
+		return this.reDraw();
+	}
+	reDraw() {
+		ctx.beginPath();
+		ctx.arc(this.x,this.y,this.r,this.sAngle,this.eAngle,this.counterclockwise);
+		ctx.strokeStyle = "blue";
+		ctx.fillStyle = "orange";
+		ctx.fill();
+		ctx.stroke();
+		ctx.closePath();
+	}
+}
