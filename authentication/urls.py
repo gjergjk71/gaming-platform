5d6492189f0d667f4a8d11c739d8c783e181ff57
gjergjk71 <gjergjk71@gmail.com>
2018-09-24 22:25:07 +0200

    Configured settings for serving static files

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 	path("login",views.custom_login,name="login"),
 	path("logout/",auth_views.logout,name="logout"),
 	path('register',views.register,name="register"),
-
-]
+	path("valid_credentials_api",views.valid_credentials_api,name="valid_credentials_api"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
