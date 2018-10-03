bf488a4f70a36501bcc71556eb194686d41a5321
gjergjk71 <gjergjk71@gmail.com>
2018-10-03 18:10:23 +0200

    Fixed bugs/changes

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-	path("game_chat/<int:game_id>",views.game_chat,name="game_chat"),
-	path("get_messages_api/<int:game_id>",views.get_messages_api,name="get_messages_api"),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
