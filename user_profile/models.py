3afca9a7e47568ff416d0fa04144d61a434ad85f
gjergjk71 <gjergjk71@gmail.com>
2018-11-03 15:31:09 +0100

    Added related_name to UserProfile.user

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-	user = models.ForeignKey(User,on_delete=models.CASCADE,unique=True)
+	user = models.ForeignKey(User,on_delete=models.CASCADE,unique=True,related_name="user")
 	profile_image = models.ImageField(upload_to="profile_photos", blank=True, null=True)
 
